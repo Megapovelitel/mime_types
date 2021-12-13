ee6df0bfe1bd70a29e3356c310058244aeeee508
Megapovelitel <megapovelitel@yandex.com>
2021-12-13 22:03:14 +0700

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const options = ["/plain", "/html", "/xml"]
+
+options.forEach(option => {
+  app.get(option, (req,res) => {
+    res.type(`text${option}`)
+
+    const file = fs.readFileSync(`files/hello_world.${req.query.type}`, 'utf-8')
+    res.send(file)
+  })
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server is up at ${PORT}`);
+});
